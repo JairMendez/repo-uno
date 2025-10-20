3480b9098606e4b9485d5a3312d1508aad6decd0
Jair Mendez <jairmendezcuenca@gmail.com>
2025-10-20 14:26:50 -0500

    modificando nombre

--- codigo.py
+++ codigo.py
@@ -1,4 +1,4 @@
-nombre = "Caleb"
+nombre = "Cuenca"
 apellido = "Mendez"
 
 saludo = "Hola "+ nombre + " "+ apellido + " ¿Como estas?"
--- codigo2.py
+++ codigo2.py
@@ -1,2 +1,2 @@
-nombre = "Jorge"
+nombre = "Jorgito"
 print("Hola" + nombre)
